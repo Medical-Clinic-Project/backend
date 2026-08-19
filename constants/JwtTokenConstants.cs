24dd04db02c6f6ccf2445e4ba4c953ff03e18d2e
Sunken3410 <kareemnajar5@gmail.com>
2026-08-19 19:19:52 +0300

    Revert "initial commit supporting auth"
    This reverts commit fee9de841f2b3bf8349ce6caf4128fde19250804.

--- constants/JwtTokenConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace backend.clinicalbackend.constants;
-
-public static class JwtTokenConstants
-{
-    public const int AccessTokenMinutes = 15;
-    public const int RefreshTokenDays = 7;
-}
--- services/Interfaces/IJwtService.cs
+++
@@ -1,9 +0,0 @@
-using backend.clinicalbackend.models;
-
-namespace backend.clinicalbackend.Services.Interfaces;
-
-public interface IJwtService
-{
-    string CreateAccessToken(User user);
-    RefreshToken CreateRefreshToken(User user);
-}
